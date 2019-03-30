f3d3b3a2e7869e7419076e659431838dd1260765
dingheng4448 <ldh_1295@hotmail.com>
2019-03-30 19:57:00 +0800

    Update view command for calendar day view

--- src/planmysem/logic/commands/ViewCommand.java
+++ src/planmysem/logic/commands/ViewCommand.java
@@ -1,15 +1,23 @@
 package planmysem.logic.commands;
 
+import static planmysem.common.Utils.getNearestDayOfWeek;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 
+import planmysem.common.Clock;
+import planmysem.common.Utils;
 import planmysem.logic.CommandHistory;
 import planmysem.model.Model;
 import planmysem.model.semester.Day;
 import planmysem.model.semester.Semester;
+import planmysem.model.slot.Slot;
 
 /**
- * Adds a person to the address book.
+ * View the planner.
  */
 public class ViewCommand extends Command {
 
@@ -17,8 +25,10 @@ public class ViewCommand extends Command {
     public static final String COMMAND_WORD_SHORT = "v";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": View month/week/day view or all details of planner."
-            + "\n\tParameters: "
-            + "\n\t\tMandatory: [viewType] [specifier]"
+            + "\n\tFormat: view [viewType] [specifier]"
+            + "\n\tParameters:"
+            + "\n\t\tMandatory: [viewType]"
+            + "\n\t\tOptional: [specifier]"
             + "\n\tExample 1: " + COMMAND_WORD
             + " month"
             + "\n\tExample 2: " + COMMAND_WORD
@@ -26,8 +36,14 @@ public class ViewCommand extends Command {
             + "\n\tExample 3: " + COMMAND_WORD
             + " week recess"
             + "\n\tExample 4: " + COMMAND_WORD
-            + " day 01/03/2019"
+            + " week"
             + "\n\tExample 5: " + COMMAND_WORD
+            + " day 01/03/2019"
+            + "\n\tExample 6: " + COMMAND_WORD
+            + " day monday"
+            + "\n\tExample 7: " + COMMAND_WORD
+            + " day"
+            + "\n\tExample 8: " + COMMAND_WORD
             + " all";
 
     private final String viewArgs;
@@ -39,7 +55,7 @@ public class ViewCommand extends Command {
     @Override
     public CommandResult execute(Model model, CommandHistory commandHistory) {
         String viewType;
-        //String viewSpecifier;
+        String viewSpecifier;
         final Semester currentSemester = model.getPlanner().getSemester();
         String output = null;
 
@@ -48,9 +64,13 @@ public class ViewCommand extends Command {
             output = "all";
         } else if ("month".equals(viewArgs)) {
             output = displayMonthView(currentSemester);
+        } else if ("day".equals(viewArgs)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String currentDate = LocalDate.now().format(formatter);
+            output = displayDayView(currentSemester, currentDate);
         } else {
             viewType = viewArgs.split(" ")[0];
-            //viewSpecifier = viewArgs.split(" ")[1];
+            viewSpecifier = viewArgs.split(" ")[1];
 
             switch (viewType) {
             case "month":
@@ -60,7 +80,7 @@ public class ViewCommand extends Command {
                 //TODO: week view
                 break;
             case "day":
-                //TODO: day view
+                output = displayDayView(currentSemester, viewSpecifier);
                 break;
             default:
                 break;
@@ -136,4 +156,60 @@ public class ViewCommand extends Command {
         return sb.toString();
     }
 
+    /**
+     * Display all slots for a given day/date.
+     */
+    private String displayDayView(Semester currentSemester, String dateOrDay) {
+        HashMap<LocalDate, Day> allDays = currentSemester.getDays();
+        StringBuilder sb = new StringBuilder();
+
+        // Parse different formats of given day/date.
+        int day = -1;
+        LocalDate givenDate = Utils.parseDate(dateOrDay);
+        if (givenDate == null) {
+            day = Utils.parseDay(dateOrDay);
+        }
+        if (day == -1 && givenDate == null) {
+            return MESSAGE_USAGE;
+        }
+        if (day != -1) {
+            givenDate = getNearestDayOfWeek(LocalDate.now(Clock.get()), day);
+        }
+        sb.append(givenDate.getDayOfWeek().name() + " , " + givenDate + "\n\n");
+
+        // Retrieve all slots for given day/date in sorted order.
+        ArrayList<Slot> allSlotsInDay = allDays.get(givenDate).getSlots();
+        Comparator<Slot> comparator = new Comparator<Slot>() {
+            @Override
+            public int compare(final Slot o1, final Slot o2) {
+                return o1.getStartTime().compareTo(o2.getStartTime());
+            }
+        };
+        allSlotsInDay.sort(comparator);
+
+        // Print each slot.
+        for (Slot slot : allSlotsInDay) {
+            sb.append("* " + slot.getStartTime());
+            sb.append(" to ");
+            sb.append(Utils.getEndTime(slot.getStartTime(), slot.getDuration()));
+
+            sb.append("\n\t" + slot.getName() + "\n");
+            sb.append("\t" + "Location: " + slot.getLocation() + "\n");
+            sb.append("\t" + "Description: " + slot.getDescription() + "\n");
+            sb.append("\n\tTags: \n");
+
+            int count = 1;
+            for (String tag : slot.getTags()) {
+                sb.append("\t");
+                sb.append(count);
+                sb.append(". ");
+                sb.append(tag);
+                count++;
+            }
+
+            sb.append("\n\n");
+        }
+
+        return sb.toString();
+    }
 }
